5261c9552335e05512b5ec367b3928fe0afd8957
Caleb Herrera <caleo010@gmail.com>
2024-05-07 15:13:41 -0400

    almost done i think fr this time

---
+++ Main Pages/galleryscript.js
@@ -0,0 +1,8 @@
+function loadContent(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
